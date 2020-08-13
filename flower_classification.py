aa4ae32fa304f1f3487e351def854ccd9e698275
Insomniac-wu <505213161@qq.com>
2020-08-13 17:28:15 +0800

    complete model

--- flower_classification.py
+++ flower_classification.py
@@ -40,3 +40,58 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
 
 class_names = train_ds.class_names
 print("class labels : ",class_names)
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+normalization_layers = layers.experimental.preprocessing.Rescaling(1./255)
+
+#define the model
+num_classes = 5
+model = Sequential([
+    layers.experimental.preprocessing.Rescaling(1./255,input_shape=(img_hight,img_width,3)),
+    layers.Conv2D(16,3,padding='same',activation='relu'),
+    layers.MaxPool2D(),
+    layers.Conv2D(32,3,padding='same',activation='relu'),
+    layers.MaxPool2D(),
+    layers.Conv2D(64,3,padding='same',activation='relu'),
+    layers.MaxPool2D(),
+    layers.Flatten(),
+    layers.Dense(128,activation='relu'),
+    layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),metrics=['accuracy'])
+model.summary()
+
+epochs = 10
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs
+)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.plot(epochs_range,acc,label='Training Accuracy')
+plt.plot(epochs_range,val_acc,label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title("Training and Validation Accuracy")
+
+plt.subplot(1,2,2)
+plt.plot(epochs_range,loss,label='Training Loss')
+plt.plot(epochs_range,val_loss,label='Validation Loss')
+plt.legend(loc="upper right")
+plt.title("Training and Validation Loss")
+plt.savefig('Training massage.png')
+plt.show()
